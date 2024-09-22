6c5a28cd7c542c354259fa05fc582917bd54984a
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2024-09-22 08:21:24 +0200

    HTML Templates ausgelagert, Bilder sind komprimiert. Alle Kritikpunkte behoben.

--- script/filter.js
+++ script/filter.js
@@ -55,25 +55,10 @@ function filterContinentAll() {
     for (let c = 0; c < menu.length; c++) {
         for (let t = 0; t < mealTypes.length; t++) {
             for (let n = 0; n < menu[c][mealTypes[t]].length; n++) {
-                mealsSections[t].innerHTML += `
-                    <div class="meals_starter_single meals_single">
-                    <div class="meals_info">
-                        <div class="single_meal_header">
-                                <h3 class="meal_title">${menu[c][mealTypes[t]][n].title}</h3>
-                                <img class="img_location" src="./assets/icon/pin.png" alt="Pin">
-                                <p>${menu[c][mealTypes[t]][n].country}</p>
-                        </div>
-                        <div class="meal_ingredients">${menu[c][mealTypes[t]][n].ingredients.join(", ")}</div>
-                        <div class="meal_price">${(menu[c][mealTypes[t]][n].price).toFixed(2).replace(".", ",")} €</div>
-                    </div>
-                    <div class="count_and_add">
-                        <div id="count_${menu[c][mealTypes[t]][n].id}" class="count_this_menu">${menu[c][mealTypes[t]][n].quantity}</div>
-                        <div class="meal_add_remove_cart" id="add_cart_${menu[c][mealTypes[t]][n].id}" onclick="saveMealInfo(event, 1)">+</div>
-                    </div>
-                </div>`
-            if (menu[c][mealTypes[t]][n].quantity > 0) {
-                document.getElementById(`count_${menu[c][mealTypes[t]][n].id}`).style.display = "flex";
-            }
+                filterContinentAllTemplate(c, t, n);
+                if (menu[c][mealTypes[t]][n].quantity > 0) {
+                    document.getElementById(`count_${menu[c][mealTypes[t]][n].id}`).style.display = "flex";
+                }
             }
         }
     }
@@ -81,26 +66,30 @@ function filterContinentAll() {
     document.getElementById("continent_title").innerHTML = "alle";
 }
 
+function filterContinentAllTemplate(c, t, n) {
+    mealsSections[t].innerHTML += `
+    <div class="meals_starter_single meals_single">
+    <div class="meals_info">
+        <div class="single_meal_header">
+                <h3 class="meal_title">${menu[c][mealTypes[t]][n].title}</h3>
+                <img class="img_location" src="./assets/icon/pin.png" alt="Pin">
+                <p>${menu[c][mealTypes[t]][n].country}</p>
+        </div>
+        <div class="meal_ingredients">${menu[c][mealTypes[t]][n].ingredients.join(", ")}</div>
+        <div class="meal_price">${(menu[c][mealTypes[t]][n].price).toFixed(2).replace(".", ",")} €</div>
+    </div>
+    <div class="count_and_add">
+        <div id="count_${menu[c][mealTypes[t]][n].id}" class="count_this_menu">${menu[c][mealTypes[t]][n].quantity}</div>
+        <div class="meal_add_remove_cart" id="add_cart_${menu[c][mealTypes[t]][n].id}" onclick="saveMealInfo(event, 1)">+</div>
+    </div>
+</div>`
+}
+
 function filterContinentSingle(c) {
     clearSections();
     for (let t = 0; t < mealTypes.length; t++) {
         for (let n = 0; n < menu[c][mealTypes[t]].length; n++) {
-            mealsSections[t].innerHTML += `
-                <div class="meals_starter_single meals_single">
-                    <div class="meals_info">
-                        <div class="single_meal_header">
-                                <h3 class="meal_title">${menu[c][mealTypes[t]][n].title}</h3>
-                                <img class="img_location" src="./assets/icon/pin.png" alt="Pin">
-                                <p>${menu[c][mealTypes[t]][n].country}</p>
-                        </div>
-                        <div class="meal_ingredients">${menu[c][mealTypes[t]][n].ingredients.join(", ")}</div>
-                        <div class="meal_price">${(menu[c][mealTypes[t]][n].price).toFixed(2).replace(".", ",")} €</div>
-                    </div>
-                    <div class="count_and_add">
-                        <div id="count_${menu[c][mealTypes[t]][n].id}" class="count_this_menu">${menu[c][mealTypes[t]][n].quantity}</div>
-                        <div class="meal_add_remove_cart" id="add_cart_${menu[c][mealTypes[t]][n].id}" onclick="saveMealInfo(event, 1)">+</div>
-                    </div>
-                </div>`
+            filterContinentSingleTemplate(t, c, n);
             if (menu[c][mealTypes[t]][n].quantity > 0) {
                 document.getElementById(`count_${menu[c][mealTypes[t]][n].id}`).style.display = "flex";
             }
@@ -108,4 +97,23 @@ function filterContinentSingle(c) {
     }
     document.getElementById("choose_continent").style.display = "none";
     document.getElementById("continent_title").innerHTML = menu[c].name;
+}
+
+function filterContinentSingleTemplate(t, c, n) {
+    mealsSections[t].innerHTML += `
+    <div class="meals_starter_single meals_single">
+        <div class="meals_info">
+            <div class="single_meal_header">
+                    <h3 class="meal_title">${menu[c][mealTypes[t]][n].title}</h3>
+                    <img class="img_location" src="./assets/icon/pin.png" alt="Pin">
+                    <p>${menu[c][mealTypes[t]][n].country}</p>
+            </div>
+            <div class="meal_ingredients">${menu[c][mealTypes[t]][n].ingredients.join(", ")}</div>
+            <div class="meal_price">${(menu[c][mealTypes[t]][n].price).toFixed(2).replace(".", ",")} €</div>
+        </div>
+        <div class="count_and_add">
+            <div id="count_${menu[c][mealTypes[t]][n].id}" class="count_this_menu">${menu[c][mealTypes[t]][n].quantity}</div>
+            <div class="meal_add_remove_cart" id="add_cart_${menu[c][mealTypes[t]][n].id}" onclick="saveMealInfo(event, 1)">+</div>
+        </div>
+    </div>`
 }
